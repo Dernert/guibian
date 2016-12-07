7214e4e1e44f00abbc5ab679fac1a77dd08189f8
Funkahdafi <ldm242@gmail.com>
2016-12-07 17:04:09 +0100

    Update: index.php, mkdir script, mkdir css

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<title>Willkommen zu guibian</title>
+	<meta name="description" content="guibian: Raspberry Pi Monitoring Tool">
+	<link href="./css/design.css" rel="stylesheet">
+</head>
+
+<body>
+<br>
+<center><h1>guibian: Ein Raspberry Pi Monitoring Tool</h1></center><br>
+<center><h4>Folge uns bei Github: <a href="https://github.com/Dernert/guibian" target=_blank>guibian</a></h4></center>
+</body>
